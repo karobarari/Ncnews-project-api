8979b2a00d142f40040f873876970700d18cc200
caro <karobarari@gmail.com>
2023-11-23 15:20:39 +0000

    add get users feature

--- app/errors.js
+++ app/errors.js
@@ -1,3 +1,9 @@
+exports.handleNotARouteError = (req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+};
+
 exports.handleNotFoundError = (err, req, res, next) => {
   if (err.code === "23503" || err.status === 404) {
     res.status(404).send({ msg: "not found!" });
@@ -10,5 +16,11 @@ exports.handleInvalidParamError = (err, req, res, next) => {
 };
 
 exports.handleServerErrors = (err, req, res, next) => {
+  console.log(err, "---------------------");
   res.status(500).send({ msg: "Internal Server Error!" });
+};
+exports.handleNotARouteError = (req, res, next) => {
+  return (error = new Error("Not a route").then(() => {
+    error.status = 404;
+  }));
 };
