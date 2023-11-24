59c60369dbd7e84985306dcbde4be6de43473acb
caro <karobarari@gmail.com>
2023-11-24 12:11:07 +0000

    add sort and order query feature

--- app/app.controllers.js
+++ app/app.controllers.js
@@ -30,12 +30,15 @@ exports.getAllTopics = (req, res, next) => {
 };
 
 exports.getArticle = (req, res, next) => {
-  const { topic } = req.query;
-  selectArticle(topic)
+  const { topic, sort_by, order } = req.query;
+  selectArticle(topic, sort_by, order)
     .then((articles) => {
       res.status(200).send({ articles });
     })
-    .catch(next);
+    .catch((err) => {
+      console.log(err, "---------------------");
+      next(err)
+    });
 };
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
