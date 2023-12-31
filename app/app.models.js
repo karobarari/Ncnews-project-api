a849473bac7d589cbc547e469c32cae1e1f84e96
caro <karobarari@gmail.com>
2023-12-31 11:05:49 +0000

    debugging the getArticle by id function

--- app/app.models.js
+++ app/app.models.js
@@ -87,12 +87,10 @@ exports.selectArticlesById = (article_id) => {
   const queryString = `
     SELECT articles.*, COUNT(comments.author) AS comment_count
     FROM articles
-    JOIN comments ON articles.article_id = comments.article_id
+    LEFT JOIN comments ON articles.article_id = comments.article_id
     WHERE articles.article_id = $1
-    GROUP BY articles.article_id
-
+    GROUP BY articles.article_id;
   `;
-
   return db.query(queryString, queryValue).then((result) => {
     if (result.rows.length === 0) {
       return Promise.reject({ status: 404, msg: "not found!" });
